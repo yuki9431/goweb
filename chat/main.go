afb1b09e149869fccdb8b8850f06375e798e6676
Hiroyuki Tomida <mac@Hiroyukis-MacBook-Air.local>
2019-05-29 20:06:22 +0900

    1章終わり

---
+++ chat/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"goWeb/trace"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse()
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("templates/css"))))
+
+	go r.run()
+
+	// Start WebServer
+	log.Println("Start WebServer port", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("*ListenAndServe:", err)
+	}
+}
---
+++ trace/tracer.go
@@ -0,0 +1,23 @@
+package trace
+
+import (
+	"fmt"
+	"io"
+)
+
+type tracer struct {
+	out io.Writer
+}
+
+type Tracer interface {
+	Trace(...interface{})
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
+	t.out.Write([]byte(fmt.Sprint("\n")))
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
