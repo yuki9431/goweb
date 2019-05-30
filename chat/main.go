c15aceb7a4d0298ea9f60b63582bddccc5f135dc
Hiroyuki Tomida <mac@Hiroyukis-MacBook-Air.local>
2019-05-30 20:22:43 +0900

    add: google認証実装でけた

--- chat/main.go
+++ chat/main.go
@@ -9,6 +9,9 @@ import (
 	"path/filepath"
 	"sync"
 	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
 )
 
 type templateHandler struct {
@@ -27,6 +30,10 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
 	flag.Parse()
+
+	// 認証のセットアップ
+	gomniauth.SetSecurityKey("test")
+	gomniauth.WithProviders(google.New("441709820078-bvl9d7p5ils5la21ftbjomj4c0ee11oq.apps.googleusercontent.com", "byi4EyHsYCtE0Xz0shUPFZcO", "http://localhost/auth/callback/google"))
 
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
